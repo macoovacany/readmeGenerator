ae4bc9401c4be1435ad08449e1430faa9fbb36c8
macoovacany <macoovacany@hotmail.com>
2021-05-02 20:45:10 +0930

    added validation for emails

--- questions.js
+++ questions.js
@@ -1,5 +1,13 @@
 const licenseData = require('./licensesReference.js');
 
+// some utiltty functions 
+
+const isEmail =function (strin)
+{
+  reEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+  return !!strin.match(reEmail)||"Does not appear to be a valid email.";
+}
+
 
 
 questions = [
@@ -53,6 +61,7 @@ questions = [
         type: 'input',
         name: 'email',
         message: 'What is your email?',
+        validate: isEmail
     },
 ]
 
